e98b14f4b39fd27578167c8beda8e230e6b50a79
Rodrigo Leocadio <rodrigo.leocadio@outlook.com>
2026-05-08 14:16:53 -0300

    fix devise mailer sender

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -30,23 +30,25 @@ Rails.application.configure do
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
 
+  app_host = ENV.fetch("APP_HOST", "www.bolaocanarinho.com")
+
   config.action_mailer.default_url_options = {
-    host: ENV.fetch("APP_HOST", "seudominio.com"),
+    host: app_host,
     protocol: "https"
   }
 
-  Rails.application.routes.default_url_options[:host] = ENV.fetch("APP_HOST", "seudominio.com")
+  Rails.application.routes.default_url_options[:host] = app_host
   Rails.application.routes.default_url_options[:protocol] = "https"
 
   config.action_mailer.delivery_method = :smtp
 
   config.action_mailer.smtp_settings = {
-    address: ENV.fetch("SMTP_ADDRESS", "smtp.gmail.com"),
+    address: ENV.fetch("SMTP_ADDRESS", "smtp-relay.brevo.com"),
     port: ENV.fetch("SMTP_PORT", 587).to_i,
-    domain: ENV.fetch("SMTP_DOMAIN", "seudominio.com"),
+    domain: ENV.fetch("SMTP_DOMAIN", "bolaocanarinho.com"),
     user_name: ENV.fetch("SMTP_USERNAME"),
     password: ENV.fetch("SMTP_PASSWORD"),
-    authentication: "plain",
+    authentication: :plain,
     enable_starttls_auto: true
   }
 
@@ -63,13 +65,10 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   # ===== HOSTS =====
-  # Descomente e ajuste quando tiver domínio definido:
+
+  # Se algum dia der erro de "Blocked host", descomente:
   #
-  # config.hosts = [
-  #   ENV.fetch("APP_HOST", "seudominio.com")
-  # ]
-  #
-  # config.host_authorization = {
-  #   exclude: ->(request) { request.path == "/up" }
-  # }
+  # config.hosts << "www.bolaocanarinho.com"
+  # config.hosts << "bolaocanarinho.com"
+  # config.hosts << "bolao-do-hexa.onrender.com"
 end
